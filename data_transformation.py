dcc9fafdee44f784b93dc79abb99918f232ff814
RJ Andaya <rodrigoandayajr.cs@gmail.com>
2024-06-02 07:32:18 -0700

    - separate csv handling from data_transformation.py - optimize - standardize_name() optimize - standardize_address() - remove process_csv_files() - make terminal logs easier to read

--- data_transformation.py
+++ data_transformation.py
@@ -3,23 +3,22 @@ Handle data transformation for Call the Car manifest CSV files
 Manipulate the pandas dataframe object of the imported CSV file
 """
 
-import os
 import re
-import pandas as pd
 
 
+# TODO: change function parameter to dataframe object. will need to modify tests.py
+# TODO: append wait_time and oxygen to new columns in dataframe
+# TODO: set origin comment to make lines 26 & 29 case insensitive without
+#   breaking regex pattern
 def extract_wait_time_and_oxygen(comment):
     """
     Returns wait time and oxygen
     Receives comment
     """
-    # TODO: change function parameter to dataframe object. will need to modify tests.py
-    # TODO: append wait_time and oxygen to new columns in dataframe
-    # TODO: set origin comment to make lines 26 & 29 case insensitive without
-    #   breaking regex pattern
 
     # Define the pattern to match
-    pattern = r"Wait time:\s*(\d+)\s*minutes(?:.*?\b(\d+)\s*(?:liter|liters|LPM|L|lts|LITERS|lt|l)\b)?"
+    pattern = r"Wait time:\s*(\d+)\s*minutes(?:.*?\b(\d+)\s*\
+        (?:liter|liters|LPM|L|lts|LITERS|lt|l)\b)?"
 
     # Remove single and double quotes from the pattern
     comment = comment.replace("'", "").replace('"', "")
@@ -49,6 +48,60 @@ def extract_wait_time_and_oxygen(comment):
     return [wait_time, oxygen]
 
 
+# TODO: modify to implement error handling
+def standardize_name(df):
+    """
+    Reads a CSV file, combines 'First Name' and 'Last Name' into 'Patient Name',
+    and saves the updated DataFrame to an output CSV file.
+    """
+
+    # Check if "First Name" and "Last Name" columns exist in the DataFrame
+    if "First Name" in df.columns and "Last Name" in df.columns:
+        # Create a new "Patient Name" column combining "Last Name" and "First Name"
+        df["Patient Name"] = (
+            df["Last Name"].str.strip() + ", " + df["First Name"].str.strip()
+        )
+
+        # Display the first few rows of the updated DataFrame to verify the changes
+        print("Updated DataFrame (standardize_name):\n", df.head(), "\n")
+
+        return df
+    else:
+        print("Columns 'First Name' and 'Last Name' are not found in the CSV file.")
+        return None
+
+
+def standardize_address(df):
+    """
+    Renames columns in the CSV:
+        "Origin Street" to "PU Address"
+        "Origin City" to "PU City"
+        "Origin State" to "PU State"
+        "Origin Postal" to "PU Zip"
+    Receives a CSV file path, reads it into a DataFrame, renames the columns,
+    and saves the updated DataFrame to an output CSV file.
+    """
+
+    # Define column mapping
+    column_mapping = {
+        "Origin Street": "PU Address",
+        "Origin City": "PU City",
+        "Origin State": "PU State",
+        "Origin Postal": "PU Zip",
+    }
+
+    # Rename the columns
+    df = df.rename(columns=column_mapping)
+
+    # Print the updated DataFrame (for debugging purposes)
+    print("Updated DataFrame (standardize_address):\n", df.head(), "\n")
+
+    return df
+
+
+# TODO: we can safely delete this function.
+# implementation above removes CSV handling
+'''
 def standardize_name(file_path):
     """
     Reads a CSV file, combines 'First Name' and 'Last Name' into 'Patient Name',
@@ -92,56 +145,12 @@ def standardize_name(file_path):
         # run stderror
         # TODO: CHANGE TO ERROR HANDLING
         return None
+'''
 
 
-def standardize_address(df):
-    """
-    Renames columns in the CSV:
-        "Origin Street" to "PU Address"
-        "Origin City" to "PU City"
-        "Origin State" to "PU State"
-        "Origin Postal" to "PU Zip"
-    Receives a CSV file path, reads it into a DataFrame, renames the columns,
-    and saves the updated DataFrame to an output CSV file.
-    """
-    # Load the DataFrame from the CSV file
-    # df = pd.read_csv(csv_file, index_col=False)
-
-    # Print the original DataFrame (for debugging purposes)
-    print("Original DataFrame (standardize_address):")
-    print(df.head())
-
-    # Define column mapping
-    column_mapping = {
-        "Origin Street": "PU Address",
-        "Origin City": "PU City",
-        "Origin State": "PU State",
-        "Origin Postal": "PU Zip",
-    }
-
-    # Rename the columns
-    df = df.rename(columns=column_mapping)
-
-    # Print the updated DataFrame (for debugging purposes)
-    print("Updated DataFrame (standardize_address):")
-    print(df.head())
-
-    # Specify the directory where you want to save the updated CSV file
-    output_folder = "output"
-    os.makedirs(
-        output_folder, exist_ok=True
-    )  # Create the directory if it doesn't exist
-
-    # Specify the path where you want to save the updated CSV file
-    output_file = os.path.join(output_folder, "updated.csv")
-
-    # Save the updated DataFrame to the output file
-    df.to_csv(output_file, index=False)
-
-    print(f"CSV saved to {output_file}")
-    return df
-
-
+# TODO: we can safely delete this function.
+# replaced with df = dt.standardize_address(dt.standardize_name(df) in main.py
+'''
 def process_csv_files(csv_file):
     """
     Calls the standardize_name and standardize_address functions with the provided file paths.
@@ -160,3 +169,4 @@ def process_csv_files(csv_file):
 
 # test function for standardze_address
 # standardize_address("import/Download(via_Manifest)_16e15fa4-b454-4c30-8a76-2945bdff6d8c.csv")
+'''
--- main.py
+++ main.py
@@ -18,17 +18,36 @@ def main():
     df = pd.read_csv(file_path, index_col=False)
 
     # Check the structure and contents of the DataFrame
-    print("Original DataFrame (standardize_name):")
-    print(df.head())
-    print(df["Origin Comments"].loc[df.index[0]])
+    print("Original DataFrame:\n", df.head(), "\n")
 
     # Example how to access Origin Comments cell
-    #
     origin_comments = df["Origin Comments"].loc[df.index[0]]
-    print(dt.extract_wait_time_and_oxygen(origin_comments))
+    print("Extracted Origin Comment:\n", origin_comments)
+    print(
+        "Extracted [wait_time, oxygen]:",
+        dt.extract_wait_time_and_oxygen(origin_comments),
+        "\n",
+    )
 
     # Run standardize_name and standardize_address
-    # dt.process_csv_files("import/Download(via_Manifest)_16e15fa4-b454-4c30-8a76-2945bdff6d8c.csv")
+    df = dt.standardize_address(dt.standardize_name(df))
+
+    # Verify column values have been updated
+    print("Verify updated columns\n", df.columns.values, "\n")
+
+    # Specify the directory where you want to save the updated CSV file
+    output_folder = "output"
+    os.makedirs(
+        output_folder, exist_ok=True
+    )  # Create the directory if it doesn't exist
+
+    # Specify the path where you want to save the updated CSV file
+    output_file = os.path.join(output_folder, "updated.csv")
+
+    # Save the updated DataFrame to the output file
+    df.to_csv(output_file, index=False)
+
+    print(f"CSV saved to {output_file}")
 
 
 if __name__ == "__main__":
